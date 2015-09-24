1431dfbf0fcf429e2dde678b6eff86d41cb8b715
Thomas Hahn <hahn@feynarts.de>
2015-09-24 12:07:37 +0200

    FeynArts 3.9; Last update: 23 Sep 15; * Fixed tiny quirk in DiagramSelect/Group/Map: selection function may now use substitution rules (a /. b) internally.

--- FeynArts/Analytic.m
+++ FeynArts/Analytic.m
@@ -2,7 +2,7 @@
 	Analytic.m
 		Translation of InsertFields output into
 		analytic expressions
-		last modified 4 Apr 15 th
+		last modified 23 Sep 15 th
 *)
 
 Begin["`Analytic`"]
@@ -772,11 +772,11 @@ DiagramDelete[ other_, n__ ] := Delete[other, ExpandRanges[n]]
 
 
 DiagramMap[ foo_, tops:(h_TopologyList)[__] ] :=
-Block[ {lev, Rule},
+Block[ {lev},
   lev = ResolveLevel[InsertionLevel /. List@@ h][[-1]];
-  Rule[_] := Sequence[];
   Apply[ #1 -> (#2 /. gr:FeynmanGraph[__, lev == _][__] :> foo[gr, #1, h])&,
       tops, 1 ] /.
+    (_[] -> Insertions[_][__]) :> Seq[] /.
     (FeynmanGraph[__][__] -> _[]) :> Seq[] /.
     (Topology[__][__] -> _[]) :> Seq[]
 ]
